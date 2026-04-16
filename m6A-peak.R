be051adbf2b4cb5f23023313b486688f42435d8c
BMILAB <33172581+BMILAB@users.noreply.github.com>
2026-04-16 16:35:33 +0800

    Rename  files

--- m6A-peak.R
+++ m6A-peak.R
@@ -1,12 +1,12 @@
 library(m6Aexpress)
-f1 <- "SRR3285589_sorted.bam"
-f2 <- "SRR3285590_sorted.bam"
-f3 <- "SRR3285591_sorted.bam"
-f4 <- "SRR3285592_sorted.bam"
-f5 <- "SRR3285593_sorted.bam"
-f6 <- "SRR3285594_sorted.bam"
-f7 <- "SRR3285595_sorted.bam"
-f8 <- "SRR3285596_sorted.bam"
+f1 <- "INPUT1_sorted.bam"
+f2 <- "IP1_sorted.bam"
+f3 <- "INPUT2_sorted.bam"
+f4 <- "IP2_sorted.bam"
+f5 <- "TREATED_INPUT1_sorted.bam"
+f6 <- "TREATED_IP1_sorted.bam"
+f7 <- "TREATED_INPUT2_sorted.bam"
+f8 <- "TREATED_IP2_sorted.bam"
 gtf="Arabidopsis_thaliana.TAIR10.59.gtf"
 
 IP_BAM <- c(f2,f4)
@@ -14,23 +14,24 @@ INPUT_BAM <- c(f1,f3)
 TREATED_IP_BAM <- c(f6,f8)
 TREATED_INPUT_BAM <- c(f5,f7)
 Get_peak_infor <- Get_peakinfor(IP_BAM,INPUT_BAM,TREATED_IP_BAM,TREATED_INPUT_BAM,GENE_ANNO_GTF = gtf,UCSC_TABLE_NAME = NA)
-save(Get_peak_infor,file = "col_ALKBH10B.rda")
+save(Get_peak_infor,file = "peak_info.rda")
 
-#col-0
+#control
 result <- exomepeak(GENE_ANNO_GTF = gtf,IP_BAM = c(f2,f4),INPUT_BAM = c(f1,f3))
-#ALKBH10B
+#treated
 result <- exomepeak(GENE_ANNO_GTF = gtf,IP_BAM = c(f6,f8),INPUT_BAM = c(f5,f7))
 
 #peak distribution
 library(Guitar)
 txdb <- makeTxDbFromGFF(file = gtf,format = "gtf")
-stBedFiles <- list('exomePeak_output_col-0/con_peak.bed',
-                   'exomePeak_output_ALKBH10B/con_peak.bed')
+stBedFiles <- list('exomePeak_output_control/con_peak.bed',
+                   'exomePeak_output_treated/con_peak.bed')
 plot <- GuitarPlot(txTxdb = txdb,
                    stBedFiles = stBedFiles,
                    headOrtail = FALSE,
                    enableCI = FALSE,
                    mapFilterTranscript = TRUE,
                    pltTxType = c("mrna"),
-                   stGroupName = c("col-0","ALKBH10B"))
+                   stGroupName = c("control","treated"))
 plot
+
